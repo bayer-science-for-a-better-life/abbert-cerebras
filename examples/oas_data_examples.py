4af90ce5aa9cea4a4a09197f87c366917d39dd38
Santi Villalba <sdvillal@gmail.com>
2021-09-08 20:40:08 +0200

    minimal bare example showing OAS unpaired data schema

---
+++ examples/oas_data_examples.py
@@ -0,0 +1,16 @@
+import json
+from pprint import pprint
+
+from abbert2.oas import OAS_TEST_DATA_PATH, from_parquet
+
+EXAMPLE_UNIT_PATH = OAS_TEST_DATA_PATH / 'unpaired' / 'json' / 'Banarjee_2017'
+EXAMPLE_UNIT_META_PATH = next(EXAMPLE_UNIT_PATH.glob('*.meta.json'))
+EXAMPLE_UNIT_SEQUENCES_PATH = next(EXAMPLE_UNIT_PATH.glob('*.parquet'))
+
+with EXAMPLE_UNIT_META_PATH.open('rt') as reader:
+    metadata = json.load(reader)
+sequences_df = from_parquet(EXAMPLE_UNIT_SEQUENCES_PATH)
+
+pprint(metadata)
+sequences_df.info()
+pprint(sequences_df.iloc[0].to_dict())
