4d9e592860720ef77f647460c325f6ebfa8b4bd5
Santi Villalba <sdvillal@gmail.com>
2021-09-21 13:35:57 +0200

    example of filtering and partitioning for ML
    plus a good deal of conveniences added to OAS and Unit

---
+++ examples/oas_consolidate_example.py
@@ -0,0 +1,5 @@
+from abbert2.oas.oas import train_validation_test_iterator
+
+for unit, chain, ml_subset, df in train_validation_test_iterator():
+    # Do whatever you want here... save to a consolidated dataset?
+    print(f'unit={unit.id} chain={chain} ml_subset={ml_subset} num_sequences={len(df)} sequences')
