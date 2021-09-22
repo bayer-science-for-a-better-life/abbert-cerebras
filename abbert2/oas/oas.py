e75f44df68e4d9be58353f498c5973ed88e33f3b
Santi Villalba <sdvillal@gmail.com>
2021-09-22 04:55:28 +0200

    broather guard

--- abbert2/oas/oas.py
+++ abbert2/oas/oas.py
@@ -742,16 +742,16 @@ def train_validation_test_iterator(
                 unit_sequences_df = unit.sequences_df(columns=used_ml_columns + used_qa_columns)
                 if unit_sequences_df is None:
                     continue  # FIXME: this happens when the parquet file is broken beyond the schema
-                if filtering is not None:
-                    try:
+                try:
+                    if filtering is not None:
                         unit_sequences_df = filtering(unit_sequences_df, chain)
-                    except KeyError:
-                        # FIXME: this happens when "has_mutated_conserved_cysteines_light" does not exist
-                        #        observed in one unit, to troubleshoot
-                        continue
-                # Drop QA columns
-                unit_sequences_df = unit_sequences_df.drop(columns=used_qa_columns)
-                yield unit, chain, ml_subset, unit_sequences_df
+                    # Drop QA columns
+                    unit_sequences_df = unit_sequences_df.drop(columns=used_qa_columns)
+                    yield unit, chain, ml_subset, unit_sequences_df
+                except KeyError:
+                    # FIXME: this happens when "has_mutated_conserved_cysteines_light" does not exist
+                    #        observed in one unit, to troubleshoot
+                    ...
 
 # --- Smoke testing
 
