082ce7372430869d51b0ff4170ff6576265800e5
Aarti Ghatkesar <aarti@cerebras.net>
2021-10-13 20:42:47 -0700

    Add argparse

--- tfrecord_scripts/create_tf_records.py
+++ tfrecord_scripts/create_tf_records.py
@@ -2,7 +2,6 @@ from abbert2.oas.oas import train_validation_test_iterator
 from abbert2.oas.oas import sapiens_like_train_val_test
 from tfrecord_scripts.utils import (
     create_bytes_feature,
-    create_float_feature,
     create_int_feature,
 )
 
@@ -11,6 +10,27 @@ import os
 import tensorflow as tf
 
 import json
+import argparse
+import sys
+
+def create_arg_parser():
+    """
+    Create parser for command line args.
+    :returns: ArgumentParser
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--src_input_folder",
+        required=True,
+        help="src_input_folder",
+    )
+    parser.add_argument(
+        "--out_tf_records_fldr",
+        required=True,
+        help="out_tf_records_fldr",
+    )
+
+    return parser
 
 
 def _preprocess_df(df):
@@ -178,5 +198,18 @@ def create_tfrecords(src_input_folder, out_tf_records_fldr):
                 json.dump(json_dict, stats_fh)
 
 
+def main():
+    """
+    Main function
+    """
+    
+    parser = create_arg_parser()
+    args = parser.parse_args(sys.argv[1:])
+
+    create_tfrecords(args.src_input_folder, args.out_tf_records_fldr)
+
+
 if __name__ == "__main__":
-    create_tfrecords("/cb/ml/aarti/bayer_sample/paired/Eccles_2020/SRR10358525_paired", out_tf_records_fldr="/cb/ml/aarti/bayer_sample_tfrecs")
+    tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.INFO)
+    main()
+    # create_tfrecords("/cb/ml/aarti/bayer_sample/paired/Eccles_2020/SRR10358525_paired", out_tf_records_fldr="/cb/ml/aarti/bayer_sample_tfrecs")
