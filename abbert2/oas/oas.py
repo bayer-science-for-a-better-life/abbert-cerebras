f6be9d78f0b1007151b3548c31d534b1ca9222f3
Santi Villalba <sdvillal@gmail.com>
2021-11-14 08:30:23 +0100

    improve oas diagnose

--- abbert2/oas/oas.py
+++ abbert2/oas/oas.py
@@ -1039,21 +1039,19 @@ def train_validation_test_iterator(
 
 def diagnose():
     """Shows some diagnose information, for example, what units have failed processing."""
-    from pprint import pprint
 
     oas = OAS()
-    # oas.populate_metadata_jsons()
+    oas.populate_metadata_jsons()
     df = oas.nice_unit_meta_df(recompute=False, normalize_species=True)
-    df.info()
 
-    units_to_redownload = sorted(unit.id for unit in df.query('needs_redownload').unit)
+    units_to_redownload = sorted('/'.join(unit.id) for unit in df.query('needs_redownload').unit)
     print('UNITS to redownload')
-    pprint(units_to_redownload)
+    print('\n'.join(units_to_redownload))
     print('-' * 80)
 
-    units_missing_processing = sorted(unit.id for unit in df.query('sequences_miss_processing').unit)
+    units_missing_processing = sorted('/'.join(unit.id) for unit in df.query('sequences_miss_processing').unit)
     print('UNITS missing processing')
-    pprint(units_missing_processing)
+    print('\n'.join(units_missing_processing))
     print('-' * 80)
 
     return units_to_redownload, units_missing_processing
