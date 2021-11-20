6f2964ee68936707306a121954562c355e18cfac
Santi Villalba <sdvillal@gmail.com>
2021-11-20 11:30:24 +0100

    fix and doctest anarci imgt insertion code handling

--- abbert2/common.py
+++ abbert2/common.py
@@ -1,7 +1,7 @@
 import re
 import datetime
 from pathlib import Path
-from typing import Union, Optional
+from typing import Union, Optional, Tuple
 
 import numpy as np
 import pandas as pd
@@ -44,22 +44,103 @@ IUPAC_CODES = (
 # --- Antibodies - ANARCI
 
 ANARCI_POSITION_REGEXP = re.compile(r'([0-9]+)([A-Z]*)')
-ANARCI_CODE2INSERTION = list(anarci_insertion_alphabet)
-ANARCI_INSERTION2CODE = {code: i + 1 for i, code in enumerate(anarci_insertion_alphabet[:-1])}
-ANARCI_INSERTION2CODE[''] = ANARCI_INSERTION2CODE[' '] = -1
+ANARCI_CODE2INSERTION = [''] + anarci_insertion_alphabet[:-1]  # N.B. swap ' ' with '' (no insertion) and move to start
+ANARCI_INSERTION2CODE = {insertion: code for code, insertion in enumerate(ANARCI_CODE2INSERTION)}
+ANARCI_INSERTION2CODE[''] = ANARCI_INSERTION2CODE[' '] = 0  # to support also ANARCI ' ' representation of no insertion
 
 
-def parse_anarci_position(position):
+def parse_anarci_position(position: str) -> Tuple[int, str]:
+    """Parses an anarci position like "112AA" into position (112) and insertion ("AA")."""
     position, insertion = ANARCI_POSITION_REGEXP.match(position).groups()
     return int(position), insertion
 
 
-def anarci_code_to_insertion(code: int):
+def anarci_code_to_insertion(code: int) -> str:
+    """Returns the insertion (e.g., "B") corresponding to the code (e.g., 2)."""
+    if code is None:
+        return ''
     return ANARCI_CODE2INSERTION[code]
 
 
-def anarci_insertion_to_code(insertion: str):
+def anarci_insertion_to_code(insertion: str) -> int:
+    """Returns the code (e.g., 2) corresponding to the insertion (e.g., "B")."""
     return ANARCI_INSERTION2CODE[insertion.strip()]
+
+
+def parse_anarci_position_to_imgt_code(position_insertion: str) -> Tuple[int, int]:
+    """
+    Parses an ANARCI position like "112A" into position (112) and insertion code in IMGT (e.g., -1).
+
+    Examples
+    --------
+    >>> parse_anarci_position_to_imgt_code('1')
+    (1, 0)
+
+    >>> parse_anarci_position_to_imgt_code('111')
+    (111, 0)
+
+    >>> parse_anarci_position_to_imgt_code('111A')
+    (111, 1)
+
+    >>> parse_anarci_position_to_imgt_code('111Z')
+    (111, 26)
+
+    >>> parse_anarci_position_to_imgt_code('111AA')
+    (111, 27)
+
+    >>> parse_anarci_position_to_imgt_code('111ZZ')
+    (111, 52)
+
+    >>> parse_anarci_position_to_imgt_code('112')
+    (112, 0)
+
+    >>> parse_anarci_position_to_imgt_code('112A')
+    (112, -1)
+
+    >>> parse_anarci_position_to_imgt_code('112Z')
+    (112, -26)
+
+    >>> parse_anarci_position_to_imgt_code('112AA')
+    (112, -27)
+
+    >>> parse_anarci_position_to_imgt_code('112ZZ')
+    (112, -52)
+    """
+    position, insertion = parse_anarci_position(position_insertion)  # '112AA' -> 112, 'AA'
+    insertion = anarci_insertion_to_code(insertion)                  # 'AA' ->
+
+    # in IMGT insertions are sorted in reverse for position 112
+    if position == 112:
+        insertion = -insertion
+
+    return position, insertion
+
+
+def anarci_imgt_code_to_insertion(code: int) -> str:
+    """Returns the insertion (e.g., "B") corresponding to the code (e.g., -2)."""
+    if code is None:
+        return ''
+    return ANARCI_CODE2INSERTION[abs(code)]
+
+
+def parse_anarci_position_aa_to_imgt_code(position_aa) -> Tuple[int, int, str]:
+    """
+    Parses a position=aa string (e.g., 112A=V) to a tuple (112, -1, "V").
+
+    Examples
+    --------
+    >>> parse_anarci_position_aa_to_imgt_code('1=A')
+    (1, 0, 'A')
+
+    >>> parse_anarci_position_aa_to_imgt_code('111ZZ=V')
+    (111, 52, 'V')
+
+    >>> parse_anarci_position_aa_to_imgt_code('112A=V')
+    (112, -1, 'V')
+    """
+    position, aa = position_aa.split('=')
+    position, insertion = parse_anarci_position_to_imgt_code(position)
+    return position, insertion, aa
 
 
 # --- JSON utils
