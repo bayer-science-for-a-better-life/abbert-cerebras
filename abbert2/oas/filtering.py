9cd5ae5c4f433e48a26e63b3c300dbbc6def1263
Santi Villalba <sdvillal@gmail.com>
2021-11-29 14:30:42 +0100

    avoid naming problems

--- abbert2/oas/filtering.py
+++ abbert2/oas/filtering.py
@@ -486,9 +486,9 @@ def filter_df(df: pd.DataFrame,
     return filtered_df, logs
 
 
-def print_filtering_logs(df, unit, filters, filtered_df, logs):
+def print_filtering_logs(initial_df, unit, filters, filtered_df, logs):
     print('-' * 80)
-    print(f'{unit.id}: from {len(df)} to {len(filtered_df)}')
+    print(f'{unit.id}: from {len(initial_df)} to {len(filtered_df)}')
     with pd.option_context('display.max_rows', None, 'display.max_columns', None):
         print(pd.DataFrame(logs)[['name', 'filtered_out', 'taken_s']])
     print('---')
