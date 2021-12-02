5893e488c4355cc9cac553811a183d089a488c54
Santi Villalba <sdvillal@gmail.com>
2021-12-02 15:36:18 +0100

    add simple partitioning method

---
+++ abbert2/oas/partitioning.py
@@ -0,0 +1,41 @@
+"""Partitioning strategies for machine learning evaluation."""
+import pandas as pd
+import xxhash
+
+
+# These would be interned anyway, but I like explicit
+SUBSET_TRAIN = 'train'
+SUBSET_VALIDATION = 'validation'
+SUBSET_TEST = 'test'
+
+
+#
+# --- Sequence based hashing
+#
+# Pros:
+#   - Simple
+#   - Fast
+#   - Reproducible "on the fly"
+#   - Will keep priors
+# Cons:
+#   - Sensitive to possible batch effects
+#     (sequencing run AKA unit, subject, study)
+#   - No cross-val
+#
+
+
+def assign_ml_subset_by_sequence_hashing(sequence: str,
+                                         seed: int = 0,
+                                         train_pct: int = 80,
+                                         val_pct: int = 10,
+                                         test_pct: int = 10):
+    assert (train_pct + val_pct + test_pct) == 100
+    if pd.isnull(sequence):
+        return None
+    # noinspection PyArgumentList
+    bucket = xxhash.xxh32_intdigest(sequence, seed=seed) % 100
+    if bucket < train_pct:
+        return SUBSET_TRAIN
+    if bucket < (train_pct + val_pct):
+        return SUBSET_VALIDATION
+    return SUBSET_TEST
